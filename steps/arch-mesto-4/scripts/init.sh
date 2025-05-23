a9213212f3fe77af312c9596f89db86e95fda388
sgladkovsky <sgladkovsky@aflt-systems.ru>
2025-05-23 14:10:26 +0300

    sprint complete

---
+++ steps/arch-mesto-4/scripts/init.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+echo "Init config_server"
+
+docker compose exec -T configSrv mongosh --quiet <<EOF
+
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
+
+exit();
+
+EOF
+
+
+echo "Init shard #1 primary"
+
+docker compose exec -T mongo_shard1a mongosh --quiet <<EOF
+
+rs.initiate(
+    {
+      _id : "shard1rs",
+      members: [
+        { _id : 0, host : "mongo_shard1a:27017" },
+        { _id : 1, host : "mongo_shard1b:27017" },
+        { _id : 2, host : "mongo_shard1c:27017" }
+      ]
+    }
+);
+exit();
+
+EOF
+
+
+
+echo "Init shard #2 primary"
+
+docker compose exec -T mongo_shard2a mongosh --quiet <<EOF
+
+rs.initiate(
+    {
+      _id : "shard2rs",
+      members: [
+        { _id : 3, host : "mongo_shard2a:27017" },
+        { _id : 4, host : "mongo_shard2b:27017" },
+        { _id : 5, host : "mongo_shard2c:27017" }
+      ]
+    }
+);
+exit();
+
+EOF
+
+echo "Add shard to router"
+
+docker compose exec -T mongo_router mongosh --quiet <<EOF
+
+sh.addShard( "shard1rs/mongo_shard1a:27017");
+sh.addShard( "shard1rs/mongo_shard1b:27017");
+sh.addShard( "shard1rs/mongo_shard1c:27017");
+sh.addShard( "shard2rs/mongo_shard2a:27017");
+sh.addShard( "shard2rs/mongo_shard2b:27017");
+sh.addShard( "shard2rs/mongo_shard2c:27017");
+
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } );
+
+EOF
+
+
+echo "Fill test data into router"
+docker compose exec -T mongo_router mongosh --quiet <<EOF
+
+console.log('Test Start')
+
+db = db.getSiblingDB('somedb');
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i});
+
+print("Test Complete " + db.helloDoc.countDocuments());
+
+EOF
